fe99626070cb0dfcb0763edffeb49455de9651cb
Ozair Couto <zaicoutox@gmail.com>
2025-12-21 21:58:30 -0300

    feat: add seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const testUser = await prisma.user.upsert({
+    where: { email: "test@example.com" },
+    update: {},
+    create: {
+      name: "Test User",
+      email: "test@example.com",
+    },
+  });
+
+  console.log({ testUser });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
