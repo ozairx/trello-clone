079592dc1e19d8929adbbdce5f94790eae99cb86
Ozair Couto <zaicoutox@gmail.com>
2025-12-21 22:29:20 -0300

    fix: update generated client path

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/client";
+import { PrismaClient } from "./generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 import { env } from "@/env";
