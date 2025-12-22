3d3925440229ebb6c130f23edf2457e017cc34a3
Ozair Couto <zaicoutox@gmail.com>
2025-12-22 14:25:05 -0300

    fix: now checkDbConnection try to execute a query

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -32,6 +32,7 @@ export async function checkDbConnection() {
 
   try {
     await db.$connect();
+    await db.$queryRaw`SELECT 1`;
     console.log("Database connection successful!");
   } catch (error) {
     console.error("Database connection failed:", error);
