8db6d3a8bbbff4a3806add8421a28470982ddec3
Ozair Couto <zaicoutox@gmail.com>
2025-12-22 21:23:56 -0300

    feat: add a username to Test User in db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,10 +3,13 @@ import { db } from "../src/lib/db";
 async function main() {
   const testUser = await db.user.upsert({
     where: { email: "test@example.com" },
-    update: {},
+    update: {
+      username: "testuser",
+    },
     create: {
       name: "Test User",
       email: "test@example.com",
+      username: "testuser",
     },
   });
 
