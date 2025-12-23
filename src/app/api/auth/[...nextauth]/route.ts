a99d031298a35b6cde12fd5f472ba8e859caab92
Ozair Couto <zaicoutox@gmail.com>
2025-12-22 21:33:27 -0300

    fix: update file with new approach from lib/auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,4 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth/options";
+import { handlers } from "@/lib/auth";
 
-const handler = NextAuth(authOptions);
+export const { GET, POST } = handlers;
 
-export { handler as GET, handler as POST };
